a3c0e8ab117d883abfef2ae1e104b182109c1a33
Anurag Anand <anuraganand789@hotmail.com>
2020-09-02 22:26:44 +0530

    Interpolation Example - display tables of contact details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Person    } from './person';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-friends';
+export class AppComponent implements OnInit{
+    nameOfMyApplication : string;
+    myPerson            : Person;
+    myFriends           : Array<Person>    = [];
+
+    ngOnInit() : void {
+       this.nameOfMyApplication     =    "Phone Directory";
+
+       this.myPerson                = new Person("Anurag", "Anand", "Bihar, India", "9993939399");
+
+       this.myFriends.push( new Person("Lex", "Fridman", "Russia", "00000-000"));
+       this.myFriends.push( new Person("Joey", "Diaz", "USA", "00000-000"));
+       this.myFriends.push( new Person("Joe", "Rogan", "Delhi, India", "00000-000"));
+       this.myFriends.push( new Person("Jeremey", "Micheal", "Delhi, India", "00000-000"));
+
+
+    }
 }
---
+++ src/app/person.ts
@@ -0,0 +1,13 @@
+export class Person {
+    firstName   : string;
+    lastName    : string;
+    address     : string;
+    phoneNumber : string;
+
+    constructor(firstName: string, lastName :string, address : string , phoneNumber: string){
+        this. firstName   = firstName;
+	this. lastName    = lastName;
+	this. address     = address;
+	this. phoneNumber = phoneNumber;
+    }
+}
