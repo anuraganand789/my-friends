fad6a1abd22c879dfedd894da2ca0888262ec505
Anurag Anand <anuraganand789@hotmail.com>
2020-09-02 22:34:30 +0530

    code formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Person    } from './person';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector       :     'app-root',
+  templateUrl    :     './app.component.html',
+  styleUrls      :     ['./app.component.css']
 })
 export class AppComponent implements OnInit{
     nameOfMyApplication : string;
@@ -16,9 +16,9 @@ export class AppComponent implements OnInit{
 
        this.myPerson                = new Person("Anurag", "Anand", "Bihar, India", "9993939399");
 
-       this.myFriends.push( new Person("Lex", "Fridman", "Russia", "00000-000"));
-       this.myFriends.push( new Person("Joey", "Diaz", "USA", "00000-000"));
-       this.myFriends.push( new Person("Joe", "Rogan", "Delhi, India", "00000-000"));
+       this.myFriends.push( new Person("Lex", "Fridman", "Russia", "00000-000")          );
+       this.myFriends.push( new Person("Joey", "Diaz", "USA", "00000-000")               );
+       this.myFriends.push( new Person("Joe", "Rogan", "Delhi, India", "00000-000")      );
        this.myFriends.push( new Person("Jeremey", "Micheal", "Delhi, India", "00000-000"));
 
 
